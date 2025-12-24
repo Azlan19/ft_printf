9e03ea8995936744768cd6dff6fd13642ab1473b
Azlan19 <omarazlan6@gmail.com>
2025-12-24 16:55:11 +0400

    (CHECKPOINT) Working logic with % sign

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,69 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 19:47:20 by oazlan            #+#    #+#             */
-/*   Updated: 2025/11/02 19:49:15 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/24 16:52:23 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int ft_printf(const char *, ...)
+#include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int format_checker(char format_specifier)
+{
+    if (format_specifier == 'c')
+        write(1, "4", 1);
+    else if (format_specifier == 's')
+        write(1, "4", 1);
+    else if (format_specifier == 'p')
+        write(1, "4", 1);
+    else if (format_specifier == 'd' || format_specifier == 'i')
+        write(1, "4", 1);
+    else if (format_specifier == 'u')
+        write(1, "4", 1);
+    else if (format_specifier == 'x')
+        write(1, "4", 1);
+    else if (format_specifier == 'X')
+        write(1, "4", 1);
+    else if (format_specifier == '%')
+        write(1, "%", 1);
+    else
+        write(1, "Format Specifier Error", 22);
+
+    return 0;
+}
+
+int	ft_printf(const char *intial_string, ...)
 {
     
+    int i;
+    
+    i = 0;
+    while(intial_string[i])
+    {
+        if (intial_string[i] == '%')
+        {
+            format_checker(intial_string[++i]);
+            i++;  
+        }
+        else
+        {
+            write(1, &intial_string[i++], 1);
+        } 
+    }
+    
+    return i;
+}
+
+
+
+int main()
+{
+    printf("\n");
+    // ft_printf("Hi my name is bob. I am %d%% years old", 4);
+    printf("\n\n");
+    printf("Number of characters printed = %d", ft_printf("Hi my name is bob. I am %d%% years old", 4));
+    printf("\n\n");
+
+    return 0;
 }
