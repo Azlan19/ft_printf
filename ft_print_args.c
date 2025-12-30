53ed62eb0ee09da48acd2e460750cbb4c5d4d61b
Azlan19 <omarazlan6@gmail.com>
2025-12-30 20:13:48 +0400

    (ADD) Add function to print strings

--- ft_print_args.c
+++ ft_print_args.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printchar.c                                     :+:      :+:    :+:   */
+/*   ft_print_args.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 18:07:54 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/30 19:50:36 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/30 20:10:13 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,18 @@ int ft_printchar(va_list *ap)
     c = va_arg(*ap, int);
     write(1,&c, 1);
     return 1;
+}
+
+int ft_printstr(va_list *ap)
+{
+    int i;
+    char *str = va_arg(*ap, char*);
+
+    i = 0;
+    while(str[i])
+    {
+        write(1, &str[i++], 1);
+    }
+    return i;
+    
 }
