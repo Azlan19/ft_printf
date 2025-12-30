8c6a4adc2c52161d42e26d1f765ec4d19ef55c26
Azlan19 <omarazlan6@gmail.com>
2025-12-30 22:49:12 +0400

    (MAIN) Change main function to properly test my ft_printf against the normal printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/02 19:47:20 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/30 20:04:18 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/30 21:15:11 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,20 @@
 
 int format_checker(char format_specifier, va_list *ap)
 {
-    int counter;
+    int d_i;
 
-    counter = 0;
     if (format_specifier == 'c') //character
-        counter += ft_printchar(ap);
+        return(ft_printchar(ap));
     else if (format_specifier == 's') //string
-        write(1, "s", 1);
+        return(ft_printstr(ap));
     else if (format_specifier == 'p') //The void * pointer argument has to be printed in hexadecimal format.
         write(1, "p", 1);
     else if (format_specifier == 'd' || format_specifier == 'i') //number (base 10)
-        write(1, "d", 1);
+    {
+        d_i = va_arg(*ap, int);
+        ft_print_num(d_i);
+        return(ft_intlen(d_i));
+    }
     else if (format_specifier == 'u') //unsigned decimal (base 10)
         write(1, "u", 1);
     else if (format_specifier == 'x') //hexadecimal lower case (base 16)
@@ -36,10 +39,9 @@ int format_checker(char format_specifier, va_list *ap)
         write(1, "X", 1);
     else if (format_specifier == '%') //percent sign '%'
         write(1, "%", 1);
-    else
-        write(1, "Format Specifier Error", 22);
-        return counter;
-    }
+        
+    return 0;
+}
     
 int	ft_printf(const char *fmt, ...)
 {
@@ -72,13 +74,19 @@ int	ft_printf(const char *fmt, ...)
 
 int main()
 {
-    int counter;
+    int normal_counter;
+    int my_counterr;
 
-    counter = 0;
+    normal_counter = 0;
+    my_counterr = 0;
     printf("\n");
-    counter = ft_printf("Hi my name is bob. I am %c '%s' years old", 'a', "Does it work??");
-    printf("\n\ncounter = %d", counter);
+    normal_counter = printf("normal printf = %c", 'h');
+    printf("\n");
+    my_counterr = ft_printf("my ft_printf  = %c", 'h');
+    printf("\n");
+    printf("\nnormal counter = %d", normal_counter);
+    printf("\nmy counter     = %d", my_counterr);
     printf("\n\n");
-    
+
     return 0;
 }
