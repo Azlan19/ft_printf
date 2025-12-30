35c4d50b8d962430cd92d7571114f55fbc5289a9
Azlan19 <omarazlan6@gmail.com>
2025-12-30 22:48:33 +0400

    (ADD) Add function to print numbers

--- ft_print_args.c
+++ ft_print_args.c
@@ -6,32 +6,79 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 18:07:54 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/30 20:10:13 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/30 21:35:20 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdarg.h>
 #include <unistd.h>
+#include <stdio.h>
 
+//format_specifier == 'c'
 int ft_printchar(va_list *ap)
 {
     int c;
-
+    
     c = va_arg(*ap, int);
     write(1,&c, 1);
     return 1;
 }
 
+//format_specifier == 's'
 int ft_printstr(va_list *ap)
 {
     int i;
     char *str = va_arg(*ap, char*);
-
+    
     i = 0;
     while(str[i])
     {
         write(1, &str[i++], 1);
     }
     return i;
-    
+}
+
+
+//format_specifier == 'd' || format_specifier == 'i'
+
+int ft_intlen(int num)
+{
+    int counter;
+
+    counter = 0;
+    if (num < 0)
+    {
+        num = -num;
+        counter++;
+    } 
+    while (num > 10)
+    {
+        num = num / 10;
+        counter++;
+    }
+
+    return ++counter;
+}
+
+void	ft_print_num(int n)
+{
+	char	nbr;
+
+	nbr = 0;
+	if (n == -2147483648)
+	{
+		write(1, "-2147483648", 11);
+		return ;
+	}
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		ft_print_num(n / 10);
+	}
+	nbr = (n % 10) + '0';
+	write(1, &nbr, 1);
 }
