96c88f59c6efb0ac0ac0ee4a4f295def6b5a9b81
Azlan19 <omarazlan6@gmail.com>
2025-12-31 18:20:20 +0400

    (CHECKPOINT) Almost working ptr function

--- ft_print_ptr.c
+++ ft_print_ptr.c
@@ -6,16 +6,20 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 17:53:16 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/31 17:55:04 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/31 18:16:22 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_printptr(va_list *ap)
+int	ft_printptr(va_list *ap)
 {
-    void *ptr;
+	long	ptr;
+	int		counter;
 
-    ptr = va_arg(ap, char*);
-    return 4;
+	counter = 0;
+	ptr = (long)va_arg(*ap, void *);
+	counter += write(1, "0x", 2);
+	ft_printhex(ptr, 'x');
+	return (counter + ft_hexlen(ptr));
 }
