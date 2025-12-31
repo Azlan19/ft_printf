30638c471902550bc6950f83c1a7ce305eac53a7
Azlan19 <omarazlan6@gmail.com>
2025-12-31 13:53:23 +0400

    (RENAME) Renamed the header file to match the subject

--- ft_printf.h
+++ ft_printf.h
@@ -1,36 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 12:58:13 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/31 13:06:39 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/31 13:49:24 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PRINTF_H
-# define PRINTF_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
 // Includes -- BEGIN
-# include <stdio.h>
 # include <stdarg.h>
+# include <stdio.h>
 # include <unistd.h>
-# include "ft_print_args.c"
 // Includes -- END
 
-int ft_printchar(va_list *ap);
-int ft_printstr(va_list *ap);
+int	format_checker(char format_specifier, va_list *ap);
+int	ft_printf(const char *fmt, ...);
 
-int ft_intlen(int num);
+int		ft_printchar(va_list *ap);
+int		ft_printstr(va_list *ap);
+
+int		ft_intlen(int num);
 
 void	ft_print_num(int n);
 
-int ft_hexlen(int num);
+int		ft_hexlen(int num);
 
-void ft_print_lower_hex(int n);
+void	ft_print_lower_hex(int n);
 
-void ft_print_upper_hex(int n);
+void	ft_print_upper_hex(int n);
 
 #endif
