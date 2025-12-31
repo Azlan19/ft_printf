eb73fc0b8d92a24671dd03f49bf12ef7d134d5c7
Azlan19 <omarazlan6@gmail.com>
2025-12-31 16:53:38 +0400

    (CLEAN) Remove unnecessary unsinged int conversion

--- ft_print_unsint.c
+++ ft_print_unsint.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/31 16:21:33 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/31 16:47:39 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/31 16:51:42 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,6 @@ int	ft_unsintlen(unsigned int num)
 	int	counter;
 
 	counter = 0;
-	if (num < 0)
-	{
-		num = 4294967296 + num;
-	}
 	while (num > 9)
 	{
 		num = num / 10;
@@ -34,10 +30,6 @@ void	ft_printunsint(unsigned int n)
 	char	nbr;
 
 	nbr = 0;
-	if (n < 0)
-	{
-		n = 4294967296 + n;
-	}
 	if (n >= 10)
 	{
 		ft_printunsint(n / 10);
