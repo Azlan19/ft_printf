09211c341a298d85bf4af411a2dccd57a18f234b
Azlan19 <omarazlan6@gmail.com>
2025-12-31 15:34:32 +0400

    (CLEAN) Rename args file to show only hex functions

--- ft_print_hex.c
+++ ft_print_hex.c
@@ -1,89 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_args.c                                    :+:      :+:    :+:   */
+/*   ft_print_hex.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 18:07:54 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/31 15:01:19 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/31 15:31:54 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-// format_specifier == 'c'
-int	ft_printchar(va_list *ap)
-{
-	int	c;
-
-	c = va_arg(*ap, int);
-	write(1, &c, 1);
-	return (1);
-}
-
-// format_specifier == 's'
-int	ft_printstr(va_list *ap)
-{
-	int		i;
-	char	*str;
-
-	str = va_arg(*ap, char *);
-	if (str == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	i = 0;
-	while (str[i])
-	{
-		write(1, &str[i++], 1);
-	}
-	return (i);
-}
-
-// format_specifier == 'd' || format_specifier == 'i'
-
-int	ft_intlen(int num)
-{
-	int	counter;
-
-	counter = 0;
-	if (num < 0)
-	{
-		num = -num;
-		counter++;
-	}
-	while (num > 10)
-	{
-		num = num / 10;
-		counter++;
-	}
-	return (++counter);
-}
-
-void	ft_print_num(int n)
-{
-	char	nbr;
-
-	nbr = 0;
-	if (n == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
-	if (n < 0)
-	{
-		write(1, "-", 1);
-		n = -n;
-	}
-	if (n >= 10)
-	{
-		ft_print_num(n / 10);
-	}
-	nbr = (n % 10) + '0';
-	write(1, &nbr, 1);
-}
 
 // format_specifier == 'x'
 int	ft_hexlen(int num)
