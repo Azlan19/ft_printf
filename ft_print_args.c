a811cb64eee119064e5766d3a13084517512d23e
Azlan19 <omarazlan6@gmail.com>
2025-12-31 12:46:32 +0400

    (ADD) Add function for upper hexadecimal. need to fix for negative numbers

--- ft_print_args.c
+++ ft_print_args.c
@@ -6,7 +6,7 @@
 /*   By: oazlan <oazlan@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/30 18:07:54 by oazlan            #+#    #+#             */
-/*   Updated: 2025/12/31 12:34:06 by oazlan           ###   ########.fr       */
+/*   Updated: 2025/12/31 12:45:54 by oazlan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,7 +88,7 @@ void	ft_print_num(int n)
 int ft_hexlen(int num)
 {
     int counter;
-
+    
     counter = 0;
     if (num < 0)
     {
@@ -100,27 +100,49 @@ int ft_hexlen(int num)
         num = num / 16;
         counter++;
     }
-
+    
     return ++counter;
 }
 
 void ft_print_lower_hex(int n)
 {
     char	nbr;
-    char	*base_16;
-
-    base_16 = "0123456789abcdef";
-
+    char	*low_base_16;
+    
+    low_base_16 = "0123456789abcdef";
+    
 	nbr = 0;
 	if (n < 0)
 	{
-		write(1, "-", 1);
+        write(1, "-", 1);
 		n = -n;
 	}
 	if (n >= 16)
 	{
-		ft_print_lower_hex(n / 16);
+        ft_print_lower_hex(n / 16);
 	}
-	nbr = base_16[(n % 16)];
+	nbr = low_base_16[(n % 16)];
+	write(1, &nbr, 1);   
+}
+
+//format_specifier == 'X'
+void ft_print_upper_hex(int n)
+{
+    char	nbr;
+    char	*up_base_16;
+    
+    up_base_16 = "0123456789ABCDEF";
+	
+    nbr = 0;
+	if (n < 0)
+	{
+        write(1, "-", 1);
+		n = -n;
+	}
+	if (n >= 16)
+	{
+        ft_print_upper_hex(n / 16);
+	}
+	nbr = up_base_16[(n % 16)];
 	write(1, &nbr, 1);   
 }
